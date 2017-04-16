c11aee073277284056b40d57b3c10ef504d74410
resessh <xuesu-li@retty.me>
2017-04-17 00:36:41 +0900

    :tada: BMI計算機を追加

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,8 @@
 import 'core-js/shim';
-import {run} from '@cycle/rxjs-run';
-import {VNode, h1, makeDOMDriver} from '@cycle/dom';
-import {Observable} from 'rxjs';
-import {DOMSource} from '@cycle/dom/rxjs-typings';
+import { run } from '@cycle/rxjs-run';
+import { VNode, CycleDOMEvent, div, h1, input, span, br, makeDOMDriver } from '@cycle/dom';
+import { Observable } from 'rxjs';
+import { DOMSource } from '@cycle/dom/rxjs-typings';
 
 type So = {
     DOM: DOMSource
@@ -13,7 +13,51 @@ type Si = {
 };
 
 function main({DOM}: So): Si {
-    const dom$ = Observable.of(h1(['It works!']));
+    const inputWeight$ = DOM.select('.weight').events('input');
+    const inputHeight$ = DOM.select('.height').events('input');
+
+    const hogeWeight$ = inputWeight$.map(
+        (e: CycleDOMEvent) => Number((e.target as HTMLInputElement).value)
+    );
+    const weight$ = Observable.merge(
+        hogeWeight$,
+        Observable.of(65)
+    );
+    const height$ = inputHeight$.map((e: CycleDOMEvent) => Number((e.target as HTMLInputElement).value)).startWith(160);
+
+    const bmi$ = Observable.combineLatest(
+        weight$,
+        height$,
+        (weight, height) => weight / height / height * 10000
+    );
+
+    const dom$ = Observable.combineLatest(
+        weight$, height$, bmi$,
+        (weight, height, bmi) =>
+            div(
+                [
+                    input('.weight', {
+                        "attrs": {
+                            'max': '100',
+                            'min': '30',
+                            'type': 'range'
+                        }
+                    }),
+                    span([weight + ' kg']),
+                    br(),
+                    input('.height', {
+                        "attrs": {
+                            'max': '220',
+                            'min': '100',
+                            'type': 'range'
+                        }
+                    }),
+                    span([height + ' cm']),
+                    br(),
+                    h1('.result', [bmi])
+                ]
+            )
+    );
 
     return {
         DOM: dom$
