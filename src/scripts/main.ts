8175d09b621fed061a44115fc2c7175e139b4dda
resessh <xuesu-li@retty.me>
2017-04-22 01:09:30 +0900

    :recycle: sliderをmainから分離

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -12,9 +12,39 @@ type Si = {
     DOM: Observable<VNode>
 };
 
+function renderWeightSlider(weight: number) {
+    return div([
+        input('.weight-slider', {
+            'attrs': {
+                max: '100',
+                min: '30',
+                type: 'range',
+                value: weight
+            }
+        }),
+        span([`Weight: ${weight}kg`]),
+        br()
+    ]);
+}
+
+function renderHeightSlider(height: number) {
+    return div([
+        input('.height-slider', {
+            'attrs': {
+                max: '220',
+                min: '130',
+                type: 'range',
+                value: height
+            }
+        }),
+        span([`Height: ${height}cm`]),
+        br()
+    ]);
+}
+
 function main({DOM}: So): Si {
-    const inputWeight$ = DOM.select('.weight').events('input');
-    const inputHeight$ = DOM.select('.height').events('input');
+    const inputWeight$ = DOM.select('.weight-slider').events('input');
+    const inputHeight$ = DOM.select('.height-slider').events('input');
 
     const hogeWeight$ = inputWeight$.map(
         (e: CycleDOMEvent) => Number((e.target as HTMLInputElement).value)
@@ -36,24 +66,8 @@ function main({DOM}: So): Si {
         (weight, height, bmi) =>
             div(
                 [
-                    input('.weight', {
-                        "attrs": {
-                            'max': '100',
-                            'min': '30',
-                            'type': 'range'
-                        }
-                    }),
-                    span([weight + ' kg']),
-                    br(),
-                    input('.height', {
-                        "attrs": {
-                            'max': '220',
-                            'min': '100',
-                            'type': 'range'
-                        }
-                    }),
-                    span([height + ' cm']),
-                    br(),
+                    renderHeightSlider(height),
+                    renderWeightSlider(weight),
                     h1('.result', [bmi])
                 ]
             )
